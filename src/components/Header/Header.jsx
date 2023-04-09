abd734d09d237951526eb8bf5d2e68cdedac2d37
hrhalim <hrhalimbd8624@gmail.com>
2023-04-09 23:21:01 +0600

    Setup project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+         <ul>
+            <lia>Home</lia>
+            <lia>Home</lia>
+            <lia>Home</lia>
+         </ul>
+        </div>
+    );
+};
+
+export default Header;
