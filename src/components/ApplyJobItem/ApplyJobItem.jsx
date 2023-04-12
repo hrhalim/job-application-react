92eb6bdd947a7a8ea6873e3b1ccfa9c0f1b27cf6
hrhalim <hrhalimbd8624@gmail.com>
2023-04-12 23:01:24 +0600

    Added filter options

--- src/components/ApplyJobItem/ApplyJobItem.jsx
+++ src/components/ApplyJobItem/ApplyJobItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 const ApplyJobItem = ({jobItem}) => {
-    const {id, image, salary, title, location, company} = jobItem; 
+    const {id, image, salary, title, location, company, jobTypes} = jobItem; 
 
     return (
         <div className='job-item border rounded-md md:flex justify-between items-center p-7 mb-12'>
@@ -15,8 +15,17 @@ const ApplyJobItem = ({jobItem}) => {
             <div className='job-content'>
                 <h3 className='text-2xl font-semibold pt-4'>{title}</h3>
                 <h4 className='pb-2'>{company}</h4>
-                <div className='btn-jobtype flex gap-4'> 
+                <div className='btn-jobtype flex gap-4'>
+                {
+                jobTypes.map((jobtype, index) =>  <span
+                    key={index}
+                     >
+                    {jobtype}
+                    
+                    </span>)
+                } 
                 </div> 
+                 
                 <div className='flex gap-4 pb-4 pt-4'>
                     <p className='flex gap-2'><span><MapPinIcon className="h-6 w-6"/></span> {location}</p>
                     <p className='flex gap-2'><span><CurrencyDollarIcon className="h-6 w-6"/></span>Salary: {salary}</p>
--- src/components/ApplyJobs/ApplyJobs.jsx
+++ src/components/ApplyJobs/ApplyJobs.jsx
@@ -1,28 +1,52 @@
+import { useState, useEffect } from 'react';
 import ApplyJobItem from '../ApplyJobItem/ApplyJobItem'; 
 import BannerArea from '../BannerArea/BannerArea';
 import './ApplyJobs.css' 
  
-import {useLoaderData } from 'react-router-dom';
-const ApplyJobs = () => {    
-    const Jobitems = useLoaderData();  
+import {useLoaderData } from 'react-router-dom';   
 
-    const handleFilter = (event) => {
-      // console.log(event.value);
-    }
+const ApplyJobs = () => {    
+    const Jobitems = useLoaderData();   
+    const [saveCart, setSaveCart] = useState(Jobitems);
+    const [filterData, setFilterData] = useState([]);  
+
+    useEffect(() => {
+      setFilterData(saveCart);
+    }, []);
+
+    const setRightData = (e) => {
+      let value = e.target.value;
+      let setArrayData = [];
+      value === "All"
+        ? (setArrayData = saveCart)
+        : saveCart.map((el) => {
+            let data = el.jobTypes.find((ell) => ell === value);
+            if (data) {
+              setArrayData.push(el);
+            }
+          });
+      setFilterData(setArrayData);
+    };
+  
     return (
         <div>
         <BannerArea>Applied Jobs</BannerArea>
         <div className='mt-20'>  
            <div className='container'>
            <div className='flex justify-end pb-8'>
-              <select onChange={() => handleFilter()} id="jobdata">
-                <option>Filter By</option>
-                <option value="Remote">Remote</option>
-                <option value="Onsite">Onsite</option>  
-              </select>
+
+           <select onChange={(e) => setRightData(e)} id="jobdata">
+              <option value="All">Filter By</option> 
+              <option value="Remote">Remote</option>
+              <option value="Full-time">Full-time</option> 
+              <option value="Onsite">Onsite</option> 
+            </select> 
+
             </div>
               {
-                Jobitems.map(jobItem => <ApplyJobItem
+                filterData &&
+                filterData.length &&
+                filterData.map(jobItem => <ApplyJobItem
                   jobItem={jobItem}
                   key={jobItem.id}
                 ></ApplyJobItem>)
